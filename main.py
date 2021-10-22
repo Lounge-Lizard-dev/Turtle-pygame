1022fcfde237545a92fb8c5f189ba6b5a4803d79
Lounge Lizard dev <86478645+Lounge-Lizard-dev@users.noreply.github.com>
2021-10-22 21:28:35 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# First we'll import some turtles and shapes: 
+from turtle import *
+from shapes import *
+
+# Create a turtle named Tommy:
+tommy = Turtle()
+tommy.shape("turtle")
+tommy.speed(7)
+
+# Draw three circles:
+draw_circle(tommy, "green", 50, 25, 0)
+draw_circle(tommy, "blue", 50, 0, 0)
+draw_circle(tommy, "yellow", 50, -25, 0)
+
+# Write a little message:
+tommy.penup()
+tommy.goto(0,-50)
+tommy.color("black")
+tommy.write("Teach With Code!", align="center", font=("Arial",16 ,"bold"))
+tommy.goto(0,-80)
+
+# Try changing draw_circle to draw_square, draw_triangle, or draw_star
+
+mainloop()
